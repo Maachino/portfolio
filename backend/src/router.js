482ad646459b95268ddbebc47ecca9900d688fa7
Maachino <maach.med@yahoo.com>
2023-07-20 18:11:27 +0200

    newBie create some backend routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,12 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
-const itemControllers = require("./controllers/itemControllers");
+const projectControllers = require("./controllers/projectControllers");
 
-router.get("/items", itemControllers.browse);
-router.get("/items/:id", itemControllers.read);
-router.put("/items/:id", itemControllers.edit);
-router.post("/items", itemControllers.add);
-router.delete("/items/:id", itemControllers.destroy);
+router.get("/projects", projectControllers.browse);
+router.get("/projects/:id", projectControllers.read);
+router.put("/projects/:id", projectControllers.edit);
+router.post("/projects", projectControllers.add);
+router.delete("/projects/:id", projectControllers.destroy);
+
+const skillControllers = require("./controllers/skillControllers");
+
+router.get("/skills", skillControllers.browse);
+router.get("/skills/:id", skillControllers.read);
+router.put("/skills/:id", skillControllers.edit);
+router.post("/skills", skillControllers.add);
+router.delete("/skills/:id", skillControllers.destroy);
 
 module.exports = router;
