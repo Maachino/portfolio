8b843b75f59fdb0ebcab4b22f1007b917051d399
Maachino <maach.med@yahoo.com>
2023-07-21 12:48:20 +0200

    add some pictures - head section and the header - some css files

---
+++ frontend/src/components/SectionHead.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line react/prop-types
+function SectionHead({ icon, title, className }) {
+  return (
+    <div className={`section__head ${className}`}>
+      <span>{icon}</span>
+      <h2>{title}</h2>
+    </div>
+  );
+}
+
+export default SectionHead;
